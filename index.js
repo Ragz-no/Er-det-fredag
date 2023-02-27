051899e96eb8f4022450402dd3a96d5ffb126e84
Ram Singh <70691805+Ragz-no@users.noreply.github.com>
2023-02-27 15:08:34 +0100

    Create index.js

---
+++ index.js
@@ -0,0 +1,38 @@
+function Fredag() {
+    let dag = new Date();
+    let answer = document.getElementById("svar");
+    let gif = document.createElement("img");
+    
+    if (dag.getDay() == 5) {
+        answer.innerHTML = "JA!!!";
+        answer.className = "yesh";
+        gif.src = "https://media2.giphy.com/media/lyq3LcSnaUND5jXcrv/giphy.gif?cid=27369a91957681516f1dd271242c44c001d9e34a21812faf&rid=giphy.gif&ct=g";
+        document.body.appendChild(gif);
+    } else {
+        answer.innerHTML = "Nei";
+        answer.className = "nesh";
+    }
+}
+
+function startTime() {
+    const today = new Date();
+    let h = today.getHours();
+    let m = today.getMinutes();
+    let s = today.getSeconds();
+    m = checkTime(m);
+    s = checkTime(s);
+    document.getElementById('klokke').innerHTML = "klokken er nå " + h + ":" + m + ":" + s;
+    setTimeout(startTime, 1000);
+}
+
+function checkTime(i) {
+    if (i < 10) {i = "0" + i};  // add zero in front of numbers < 10
+    return i;
+}
+
+
+
+
+
+
+
